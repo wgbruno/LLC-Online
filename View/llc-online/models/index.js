16a7729e120d6f1138ad214f51073db611094bfe
mitchb25j <mabardsley@loyola.edu>
2023-02-24 20:21:01 -0500

    Added mongoose exports

--- View/llc-online/models/index.js
+++ View/llc-online/models/index.js
@@ -1,1 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
+exports.connect = async function(){
+    let uri = process.env.DB_URI; //production DB
+
+    await mongoose.connect(uri,function(err){
+        if(err) console.log(err);
+    });
+}
+
+exports.disconnect = async function(){
+    await mongoose.connection.close();
+}
