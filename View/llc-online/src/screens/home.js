8070eacdf4179fb96092efcdee80a54abb660748
wgbruno <wgbruno@loyola.edu>
2023-02-28 11:30:47 -0500

    Rearranging Directories

--- View/llc-online/src/screens/home.js
+++ View/llc-online/src/screens/home.js
@@ -1,9 +1,9 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Gallery from "./components/Gallery";
-import Hero from "./components/hero";
+import NavBar from "../components/NavBar"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Gallery from "../components/Gallery";
+import Hero from "../components/hero";
 
 export default function Home(){
     return (
