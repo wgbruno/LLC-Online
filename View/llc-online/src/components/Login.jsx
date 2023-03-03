2ffaae58c05c12f287f3f86aa593794ce4bc6cc3
mitchb25j <mabardsley@loyola.edu>
2023-03-03 18:45:25 -0500

    updated login and registration to show on front page

--- View/llc-online/src/components/Login.jsx
+++ View/llc-online/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export const Login = () =>
+export default function Login()
 {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
--- View/llc-online/src/components/Register.jsx
+++ View/llc-online/src/components/Register.jsx
@@ -1,4 +1,4 @@
-export const Register = () =>
+export default function Register()
 {
     return
     (
