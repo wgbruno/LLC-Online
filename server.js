f446d579f77673e9f3b211530dfcd30135538845
mitchb25j <mabardsley@loyola.edu>
2023-03-03 01:10:09 -0500

    moved db start to server file

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const db_conn = require('./models/index.js');
+
+const mongoose = require('mongoose');
+
+exports.connect = async function(){
+    let uri = process.env.DB_URI; //production DB
+
+    await mongoose.connect(uri,function(err){
+        if(err) console.log(err);
+    });
+}
+
+exports.disconnect = async function(){
+    await mongoose.connection.close();
+}
