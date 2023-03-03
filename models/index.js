5069b154aa4bdbf3923cfff4cd0e6ee617402236
mitchb25j <mabardsley@loyola.edu>
2023-03-03 16:19:31 -0500

    successfully added correct db uri

--- models/index.js
+++ models/index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 exports.connect = async function(){
     let uri = process.env.DB_URI; // production DB
+    if(where==='test') uri = process.env.TESTDB_URI; //Test DB
+    if(process.env.CI) uri = 'mongodb+srv://mabardsley:Blue6907@cluster0.winsvzg.mongodb.net/?retryWrites=true&w=majority'; //MongoDB Cluster0
 
     await mongoose.connect(uri,function(err){
         if(err) console.log(err);
