8166c6a8f4ba3be837c9a5d94b3bd54c8d217711
mitchb25j <mabardsley@loyola.edu>
2023-03-03 22:12:12 -0500

    fixed home.js for main, syntax errors, and updated login page #2 #9

--- View/llc-online/src/screens/Home.js
+++ View/llc-online/src/screens/Home.js
@@ -4,8 +4,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Gallery from "../components/Gallery";
 import Hero from "../components/Hero";
-import Login from "../components/Login";
-import Register from "../components/Register";
 
 export default function Home(){
     return (
@@ -13,8 +11,6 @@ export default function Home(){
           <NavBar />
           <Header /> 
           <Hero />
-          <Login />
-          <Register />
           <Gallery />
           <Footer />
       </div>
