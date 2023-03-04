733db2ba8841d010dba075cbd1ae2802b380b165
mitchb25j <mabardsley@loyola.edu>
2023-03-03 22:05:56 -0500

    reinstated login file for main #2 #8

--- View/llc-online/src/components/Login.js
+++ View/llc-online/src/components/Login.js
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 
-export default function Login()
-{
+export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
-    const manageSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log( email );
+        console.log(email);
     }
 
     return (
-        <form>
-            <label for="email">email</label>
-            <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label for="password">password</label>
-            <input value={pass} type="password" placeholder="**********" id="password" name="password" />
-            <button>Log In</button>
-        </form>
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
     )
 }
