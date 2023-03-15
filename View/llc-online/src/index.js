88af08c304ccac5bb355030c466f05afcb457f2b
wgbruno <wgbruno@loyola.edu>
2023-03-15 14:44:00 -0400

    Fixed BrowserRouter

--- View/llc-online/src/index.js
+++ View/llc-online/src/index.js
@@ -7,27 +7,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./screens/Home";
 import LoginPage from './screens/loginpage';
 
-
-ReactDOM.render(
+ReactDOM.render((
     <BrowserRouter>
         <Routes>
-            <Route path="/home" element={<Home/>} />
-            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
         </Routes>
     </BrowserRouter>
-    ,
-    document.getElementById('root'));
-
-
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <Home />
-      <LoginPage />
-  </React.StrictMode>
-)
-*/
+    ), document.getElementById('root')
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
