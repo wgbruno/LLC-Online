bb7b3ba2e510b9ec64e75dd8f1e8e8535f8e95f9
mitchb25j <mabardsley@loyola.edu>
2023-04-23 21:11:03 -0400

    Adjusted register component #2

--- View/llc-online/src/components/Register.js
+++ View/llc-online/src/components/Register.js
@@ -1,27 +1,47 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import { loginStyles, loginLabelStyles, loginInputStyles, loginButtonStyles, loginLinkStyles } from "../style/Login.style";
 import axios from 'axios';
 
 export const Register = (props) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
-  const [name, setName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      await axios.post('http://localhost:3000/login', { email, password: pass, first_name: name, last_name: '' });
-    } catch (err) {
-      console.error(err);
-    }
+    const baseURL = "http://localhost:4000/account";
+        
+    let user = {
+      email,
+      password: pass,
+      first_name: firstName,
+      last_name: lastName
+    };
+
+    console.log(JSON.stringify(user));
+  
+    axios.post(baseURL, user)
+      .then((response) => {
+        let path = '/login';
+        navigate(path);
+      })
+      .catch((error) => {
+        console.log('Registration Error');
+        console.log(error);
+      });
   }
 
   return (
     <div style={loginStyles} className="auth-form-container">
       <h2>Register</h2>
       <form className="register-form" onSubmit={handleSubmit}>
-        <label style={loginLabelStyles} htmlFor="name">Full Name:</label>
-        <input style={loginInputStyles} value={name} name="name" onChange={(e) => setName(e.target.value)} placeholder="John Doe" id="name"/>
+        <label style={loginLabelStyles} htmlFor="firstName">First Name:</label>
+        <input style={loginInputStyles} value={firstName} name="firstName" onChange={(e) => setFirstName(e.target.value)} placeholder="John" id="firstName"/>
+        <label style={loginLabelStyles} htmlFor="lastName">Last Name:</label>
+        <input style={loginInputStyles} value={lastName} name="lastName" onChange={(e) => setLastName(e.target.value)} placeholder="Doe" id="lastName"/>
         <label style={loginLabelStyles} htmlFor="email">Email:</label>
         <input style={loginInputStyles} value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
         <label style={loginLabelStyles} htmlFor="password">Password:</label>
